f78d87b8f07f8d472e0b5931e0fbcd8bab0f031b
riteshsoni123 <comedybuddy1@gmail.com>
2021-08-24 14:58:14 +0530

    added the feature that prints the flight between country on the console

--- flight/flight.js
+++ flight/flight.js
@@ -1,6 +1,6 @@
 
 
-let button = document.querySelector('.btn');
+let button = document.querySelector('.countryinput');
 let origin = document.querySelector('.origin');
 let destination = document.querySelector('.destination');
 
@@ -35,9 +35,9 @@ tmp();
 // destination.value = "US";
 
 
-btn.addEventListener('click', function () {
+countryinput.addEventListener('click', function () {
 
-	fetch('./countries.json')
+	fetch('../countries.json')
 		.then(res => res.json())
 		.then(data => {
 			country = data;
@@ -47,114 +47,118 @@ btn.addEventListener('click', function () {
 
 				if (origin.value == country[i]['name']) {
 					origin.value = country[i]['code'];
-					console.log(country[i]['code'])
+					// console.log(country[i]['code'])
 					// console.log(origin.value);
 				}
-				else if (destination.value == country[i]['name']) {
+				if (destination.value == country[i]['name']) {
 					destination.value = country[i]['code'];
-					console.log(country[i]['code']);
+					// console.log(country[i]['code']);
 					// console.log(destination.value);
 				}
 
 			}
-		});
-	// console.log(origin.value);
-	// console.log(destination.value);
 
-	fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/${origin.value}/INR/${origin.value}/${origin.value}/${destination.value}/2021-08-21?inboundpartialdate=2021-08-21`, {
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-key": "05dce523ffmshb8fc17a05f8c86ap156854jsn4d098c6ba72f",
-			"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
-		}
-	})
-		.then(resp => resp.json())
-		.then(result => {
-			console.log(result);
-			let count = 0;
-			let routelength = result['Routes']['length'];
-			routelength = Number(routelength);
-			for (let i = 0; i < routelength; i++) {
 
-				let destinationId = result["Routes"][i]["DestinationId"];
-				let price = result["Routes"][i]["Price"];
-				let datetime = result["Routes"][i]["QuoteDateTime"];
 
-				if (price != undefined) {
-
-					console.log("Number=", count);
-					console.log("price=", price);
-
-					let quoteId = result["Routes"][i]["QuoteIds"][0];
-					let quotelength = result["Quotes"]["length"];
-
-					quotelength = Number(quotelength);
-
-					for (let j = 0; j < quotelength; j++) {
-
-						if (quoteId == result['Quotes'][j]['QuoteId']) {
-
-							let departureDate = result['Quotes'][j]['OutboundLeg']['DepartureDate'];
-							let originId = result['Quotes'][j]['OutboundLeg']['OriginId'];
-							let carrierId = result['Quotes'][j]['OutboundLeg']['CarrierIds'][0];
-							let placelength = result['Places']['length'];
-
-							console.log("DepartureDate=", departureDate);
-
-							placelength = Number(placelength);
-
-							for (let k = 0; k < placelength; k++) {
-
-								if (destinationId == result['Places'][k]['PlaceId']) {
-
-									let destinationCity = result['Places'][k]['CityName'];
-									let destinationCountry = result['Places'][k]['CountryName'];
-
-									console.log("DestinationCity=", destinationCity, destinationId);
-									console.log("DestinatonCountry=", destinationCountry);
-
-								}
-								else if (originId == result['Places'][k]['PlaceId']) {
-
-									let originCity = result['Places'][k]['CityName'];
-									let originCountry = result['Places'][k]['CountryName'];
-
-									console.log("OriginCity=", originCity);
-									console.log("OriginCountry=", originCountry);
-
-								}
-								else {
-									continue;
-								}
-							}
-
-							let carrierlength = result['Carriers']['length'];
-
-							carrierlength = Number(carrierlength);
-
-							for (let k = 0; k < carrierlength; k++) {
-
-								if (carrierId == result['Carriers'][k]['CarrierId']) {
-
-									let carrierName = result['Carriers'][k]['Name'];
-									console.log("CarrierName=", carrierName);
-									// console.log("\n");
-
-								}
-							}
-
-						}
-					}
-					count++;
-					// if (count == 8) {
-					// 	break;
-					// }
-					console.log("\n");
+			fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/${origin.value}/INR/${origin.value}/${origin.value}/${destination.value}/2021-08-28?inboundpartialdate=2021-08-28`, {
+				"method": "GET",
+				"headers": {
+					"x-rapidapi-key": "05dce523ffmshb8fc17a05f8c86ap156854jsn4d098c6ba72f",
+					"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
 				}
+			})
+				.then(resp => resp.json())
+				.then(result => {
+					console.log(result);
+					let count = 0;
+					let routelength = result['Routes']['length'];
+					routelength = Number(routelength);
+					for (let i = 0; i < routelength; i++) {
 
-			}
+						let destinationId = result["Routes"][i]["DestinationId"];
+						let price = result["Routes"][i]["Price"];
+						let datetime = result["Routes"][i]["QuoteDateTime"];
+
+						if (price != undefined) {
+
+							console.log("Number=", count);
+							console.log("price=", price);
+
+							let quoteId = result["Routes"][i]["QuoteIds"][0];
+							let quotelength = result["Quotes"]["length"];
+
+							quotelength = Number(quotelength);
+
+							for (let j = 0; j < quotelength; j++) {
+
+								if (quoteId == result['Quotes'][j]['QuoteId']) {
+
+									let departureDate = result['Quotes'][j]['OutboundLeg']['DepartureDate'];
+									let originId = result['Quotes'][j]['OutboundLeg']['OriginId'];
+									let carrierId = result['Quotes'][j]['OutboundLeg']['CarrierIds'][0];
+									let placelength = result['Places']['length'];
+
+									console.log("DepartureDate=", departureDate);
+
+									placelength = Number(placelength);
+
+									for (let k = 0; k < placelength; k++) {
+
+										if (destinationId == result['Places'][k]['PlaceId']) {
+
+											let destinationCity = result['Places'][k]['CityName'];
+											let destinationCountry = result['Places'][k]['CountryName'];
+
+											console.log("DestinationCity=", destinationCity, destinationId);
+											console.log("DestinatonCountry=", destinationCountry);
+
+										}
+										else if (originId == result['Places'][k]['PlaceId']) {
+
+											let originCity = result['Places'][k]['CityName'];
+											let originCountry = result['Places'][k]['CountryName'];
+
+											console.log("OriginCity=", originCity);
+											console.log("OriginCountry=", originCountry);
+
+										}
+										else {
+											continue;
+										}
+									}
+
+									let carrierlength = result['Carriers']['length'];
+
+									carrierlength = Number(carrierlength);
+
+									for (let k = 0; k < carrierlength; k++) {
+
+										if (carrierId == result['Carriers'][k]['CarrierId']) {
+
+											let carrierName = result['Carriers'][k]['Name'];
+											console.log("CarrierName=", carrierName);
+											// console.log("\n");
+
+										}
+									}
+
+								}
+							}
+							count++;
+							// if (count == 8) {
+							// 	break;
+							// }
+							console.log("\n");
+						}
+
+					}
+
+				});
+
 
 		});
+
+
 
 });
 
