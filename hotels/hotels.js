c00d806e27e6f3296007fc0b1a20b587354ae6bc
riteshsoni123 <comedybuddy1@gmail.com>
2021-09-14 00:07:00 +0530

    took the input from the user for hotels details

--- hotels/hotels.js
+++ hotels/hotels.js
@@ -1,6 +1,9 @@
 
 let cityinfo = document.querySelector('.city');
 let btn = document.querySelector('.submitCity');
+let checkinDateInfo=document.querySelector('.checkinDate');
+let checkoutDateInfo=document.querySelector('.checkoutDate');
+let adultsno=document.querySelector('.adults');
 
 
 
@@ -15,8 +18,45 @@ submitCity.addEventListener('click', function () {
         .then(resp => resp.json())
         .then(dataHotels => {
             console.log(dataHotels);
-            let hotels=dataHotels['suggestions'][1]['entities'];
+            let hotels = dataHotels['suggestions'][1]['entities'];
             console.log(hotels);
+
+            console.log(checkoutDateInfo.value);
+            console.log(checkinDateInfo.value);
+            console.log(hotels[0]['destinationId']);
+            console.log(adultsno.value);
+            // console.log();
+
+            for (let i = 0; i < 4; i++) {
+
+                fetch(`https://hotels-com-provider.p.rapidapi.com/v1/hotels/booking-details?checkout_date=${checkoutDateInfo.value}&hotel_id=${hotels[i]['destinationId']}&currency=INR&locale=en_US&checkin_date=${checkinDateInfo.value}&adults_number=${adultsno.value}&children_ages=4%2C0`, {
+                    "method": "GET",
+                    "headers": {
+                        "x-rapidapi-host": "hotels-com-provider.p.rapidapi.com",
+                        "x-rapidapi-key": "05dce523ffmshb8fc17a05f8c86ap156854jsn4d098c6ba72f"
+                    }
+                })
+                    .then(resp => resp.json())
+                    .then(dataHotels => {
+                        // console.log(dataHotels);
+                        let address = dataHotels['address']['fullAddress'];
+                        let overview = dataHotels['overview']['overviewSections'];
+                        let roomTypeNames = dataHotels['roomTypeNames'];
+                        let rooms = dataHotels['roomsAndRates']['rooms'];
+                        let price = dataHotels['featuredPrice']['currentPrice']['formatted'];
+                        let starrating = dataHotels['starRatingTitle'];
+
+                        console.log(address);
+                        console.log(overview);
+                        console.log(roomTypeNames);
+                        console.log(rooms);
+                        console.log(price);
+                        console.log(starrating);
+
+                    });
+            }
+
+
         });
 });
 
@@ -29,28 +69,3 @@ submitCity.addEventListener('click', function () {
 // price
 // address
 
-// fetch("https://hotels-com-provider.p.rapidapi.com/v1/hotels/booking-details?checkout_date=2022-03-27&hotel_id=2233324416&currency=INR&locale=en_US&checkin_date=2022-03-26&adults_number=1&children_ages=4%2C0", {
-//     "method": "GET",
-//     "headers": {
-//         "x-rapidapi-host": "hotels-com-provider.p.rapidapi.com",
-//         "x-rapidapi-key": "05dce523ffmshb8fc17a05f8c86ap156854jsn4d098c6ba72f"
-//     }
-// })
-//     .then(resp => resp.json())
-//     .then(dataHotels => {
-//         console.log(dataHotels);
-//         let address=dataHotels['address']['fullAddress'];
-//         let overview=dataHotels['overview']['overviewSections'];
-//         let roomTypeNames=dataHotels['roomTypeNames'];
-//         let rooms=dataHotels['roomsAndRates']['rooms'];
-//         let price=dataHotels['featuredPrice']['currentPrice']['formatted'];
-//         let starrating=dataHotels['starRatingTitle'];
-
-//         console.log(address);
-//         console.log(overview);
-//         console.log(roomTypeNames);
-//         console.log(rooms);
-//         console.log(price);
-//         console.log(starrating);
-
-//     });
