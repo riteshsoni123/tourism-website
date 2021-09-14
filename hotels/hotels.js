bc791180ee80dad53f5f0c2091774fed11c0f551
riteshsoni123 <comedybuddy1@gmail.com>
2021-09-14 12:31:58 +0530

    added the api for important places and restaurents and configured hotels api

--- hotels/hotels.js
+++ hotels/hotels.js
@@ -1,9 +1,9 @@
 
 let cityinfo = document.querySelector('.city');
 let btn = document.querySelector('.submitCity');
-let checkinDateInfo=document.querySelector('.checkinDate');
-let checkoutDateInfo=document.querySelector('.checkoutDate');
-let adultsno=document.querySelector('.adults');
+let checkinDateInfo = document.querySelector('.checkinDate');
+let checkoutDateInfo = document.querySelector('.checkoutDate');
+let adultsno = document.querySelector('.adults');
 
 
 
@@ -38,18 +38,19 @@ submitCity.addEventListener('click', function () {
                 })
                     .then(resp => resp.json())
                     .then(dataHotels => {
-                        // console.log(dataHotels);
+                        console.log(i);
+                        console.log(dataHotels);
                         let address = dataHotels['address']['fullAddress'];
                         let overview = dataHotels['overview']['overviewSections'];
                         let roomTypeNames = dataHotels['roomTypeNames'];
-                        let rooms = dataHotels['roomsAndRates']['rooms'];
+                        // let rooms = dataHotels['roomsAndRates']['rooms'];
                         let price = dataHotels['featuredPrice']['currentPrice']['formatted'];
                         let starrating = dataHotels['starRatingTitle'];
 
                         console.log(address);
                         console.log(overview);
                         console.log(roomTypeNames);
-                        console.log(rooms);
+                        // console.log(rooms);
                         console.log(price);
                         console.log(starrating);
 
@@ -61,11 +62,32 @@ submitCity.addEventListener('click', function () {
 });
 
 
+// checkoutDateInfo.value='2021-09-20';
+// checkinDateInfo.value='2021-09-15';
+// adultsno.value='1';
+// fetch(`https://hotels-com-provider.p.rapidapi.com/v1/hotels/booking-details?checkout_date=${checkoutDateInfo.value}&hotel_id=239764&currency=INR&locale=en_US&checkin_date=${checkinDateInfo.value}&adults_number=${adultsno.value}&children_ages=4%2C0`, {
+//     "method": "GET",
+//     "headers": {
+//         "x-rapidapi-host": "hotels-com-provider.p.rapidapi.com",
+//         "x-rapidapi-key": "05dce523ffmshb8fc17a05f8c86ap156854jsn4d098c6ba72f"
+//     }
+// })
+//     .then(resp => resp.json())
+//     .then(dataHotels => {
+//         // console.log(i);
+//         console.log(dataHotels);
+//         let address = dataHotels['address']['fullAddress'];
+//         let overview = dataHotels['overview']['overviewSections'];
+//         let roomTypeNames = dataHotels['roomTypeNames'];
+//         // let rooms = dataHotels['roomsAndRates']['rooms'];
+//         let price = dataHotels['featuredPrice']['currentPrice']['formatted'];
+//         let starrating = dataHotels['starRatingTitle'];
 
+//         console.log(address);
+//         console.log(overview);
+//         console.log(roomTypeNames);
+//         // console.log(rooms);
+//         console.log(price);
+//         console.log(starrating);
 
-// overviewselection
-// roomTypeNames
-// starrating
-// price
-// address
-
+//     });
---
+++ places/places.js
@@ -0,0 +1,49 @@
+
+var button = document.querySelector('.btn');
+var inputvalue = document.querySelector('.inputvalue');
+
+btn.addEventListener('click', function () {
+
+    let clintid = "L2LEFRWKXUWBUYFATD3B1KZHYSEWT1TKLL3ARBVK04EXFPEW";
+    let clintsecret = "3JNNOQPCFFCPVECEQBRHW0XBVOLAKW25CSVARSNOEJJOD4FY";
+
+    fetch(`https://api.foursquare.com/v2/venues/explore?cat=arts&mode=url&near=${inputvalue.value}&client_id=${clintid}&client_secret=${clintsecret}&v=20210101`
+    ).then(resp => resp.json())
+        .then(result => {
+
+            console.log(result);
+            var length = result['response']['groups'][0]['items']['length'];
+            console.log(Number(length));
+
+            for (let i = 0; i < 5; i++) {
+
+                let tmp = result['response']['groups'][0]['items'][i]['venue'];
+                let address = tmp['location']['formattedAddress'];
+                let id = tmp['id'];
+
+                fetch(`https://api.foursquare.com/v2/venues/${id}/photos?&client_id=${clintid}&client_secret=${clintsecret}&v=20210101`
+                ).then(resp => resp.json())
+                    .then(photo => {
+
+                        console.log(photo);
+                        console.log(photo['response']['photos']['items'][0]['source']);
+
+                        let source = photo['response']['photos']['items'][0];
+                        let prefix = source['prefix'];;
+                        let suffix = source['suffix'];;
+                        let link = `${prefix}300x300${suffix}`;
+
+                        console.log(link);
+
+                        let container=document.getElementById("contain");
+                        let div=document.createElement("div");
+                        let img=document.createElement("img");
+                        img.src=link;
+                        container.appendChild(div);
+                        div.appendChild(img);
+                        
+                    });
+
+            }
+        });
+});
