20bc43a364041e13ebfb7f6263a5a231055b79d5
riteshsoni123 <comedybuddy1@gmail.com>
2021-09-16 15:30:18 +0530

    styled the section in hotel page where information in displayed

--- places/places.js
+++ places/places.js
@@ -17,22 +17,32 @@ btn.addEventListener('click', function () {
 
             for (let i = 0; i < 5; i++) {
 
-                let tmp = result['response']['groups'][0]['items'][i]['venue'];
-                let address = tmp['location']['formattedAddress'];
-                let id = tmp['id'];
+                let venue = result['response']['groups'][0]['items'][i]['venue'];
+                // let address = venue['location']['formattedAddress'];
+                let id = venue['id'];
+                let name=venue['name'];
+                let address="";
+                let addresslength=venue['location']['formattedAddress']['length'];
+                addresslength=Number(addresslength);
+                for(let j=0;j<addresslength;j++){
+                    address=address+venue['location']['formattedAddress'][j]+", ";
+                }
+
 
                 fetch(`https://api.foursquare.com/v2/venues/${id}/photos?&client_id=${clintid}&client_secret=${clintsecret}&v=20210101`
                 ).then(resp => resp.json())
                     .then(photo => {
 
-                        console.log(photo);
-                        console.log(photo['response']['photos']['items'][0]['source']);
+                        // console.log(photo);
+                        // console.log(photo['response']['photos']['items'][0]['source']);
 
                         let source = photo['response']['photos']['items'][0];
                         let prefix = source['prefix'];;
                         let suffix = source['suffix'];;
                         let link = `${prefix}300x300${suffix}`;
 
+                        console.log(name)
+                        console.log(address);
                         console.log(link);
 
                         let container = document.getElementById("contain");
